658cd290679f00402b7d467bdc57b88c09c48a67
Hugo Silva <hugohenry57@gmail.com>
2023-02-20 23:05:44 -0300

    Cronômetro simples

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatch
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Start(10);
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S - Contar em segundos => 10s = 10 segundos");
+            Console.WriteLine("M - Contar em minutos => 1m = 1 minuto");
+            Console.WriteLine("0 - Finalizar");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+            PreStart(time * multiplier);
+
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+
+        }
+
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("StopWatch Finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
+
